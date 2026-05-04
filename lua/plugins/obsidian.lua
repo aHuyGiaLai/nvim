1854317b2b044861955a1795f4076a044cdf0f63
aHuyGiaLai <vogiahuy216@gmail.com>
2026-05-04 22:17:23 +0700

    obsidian update

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -1,47 +1,49 @@
 return {
-    "epwalsh/obsidian.nvim",
-    version = "*",
-    lazy = true,
-    ft = "markdown",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-    },
-    opts = {
-        workspaces = {
-            name = "GaHyVault",
-            path = "/mnt/data/obsiGaHy/Main Vault - dùng chung"
-        },
-    },
-    notes_subdir = "inbox",
-    new_notes_location = "notes_subdir",
+	"epwalsh/obsidian.nvim",
+	version = "*",
+	lazy = true,
+	ft = "markdown",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+	},
+	opts = {
+		workspaces = {
+			{
+				name = "GaHyVault",
+				path = "/mnt/data/obsiGaHy/Main Vault - dùng chung",
+			},
+		},
+		notes_subdir = "inbox",
+		new_notes_location = "notes_subdir",
 
-    disable_frontmatter = true,
+		disable_frontmatter = true,
 
-    templates = {
-        subdir = "templates",
-        date_format = "%Y-%m-%d",
-        time_format = "%H:%M:%S",
-    },
+		templates = {
+			subdir = "templates",
+			date_format = "%Y-%m-%d",
+			time_format = "%H:%M:%S",
+		},
 
-    mappings = {
-        ["gf"] = {
-            action = function()
-                return require("obsidian").util.gf_passthrough()
-            end,
-            opts = { noremap = false, expr = true, buffer = true },
-        },
-        ["<leader>ti"] = {
-            action = function()
-                return require("obsidian").util.toggle_checkbox()
-            end,
-            opts = { buffer = true },
-        },
-    },
+		mappings = {
+			["gf"] = {
+				action = function()
+					return require("obsidian").util.gf_passthrough()
+				end,
+				opts = { noremap = false, expr = true, buffer = true },
+			},
+			["<leader>ti"] = {
+				action = function()
+					return require("obsidian").util.toggle_checkbox()
+				end,
+				opts = { buffer = true },
+			},
+		},
 
-    completion = {
-        nvim_cmp = true,
-        min_chars = 2,
-    },
+		completion = {
+			nvim_cmp = true,
+			min_chars = 2,
+		},
 
-    ui = { enable = false }, -- Render-markdown.nvim sẽ đảm nhận phần UI
+		ui = { enable = false }, -- Render-markdown.nvim sẽ đảm nhận phần UI
+	},
 }
